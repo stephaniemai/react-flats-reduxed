3c6472579021b7bc99f7343013557b6f50d9954e
stephaniemai <stephaniemai.jacques@gmail.com>
2019-05-09 18:20:59 +0200

    display flats of the flatlist

--- src/containers/flat_list.jsx
+++ src/containers/flat_list.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import Flat from '../components/flat';
 
 class FlatList extends Component {
+
+  static defaultProps = {
+    flats: [{
+      name: "Charm at the Steps of Montmartre",
+      imageUrl: "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+      price: 164,
+      priceCurrency: "EUR"
+    }]
+  }
+
   render() {
     return (
-      <div className="col-sm-7">
-        TODO
-
+      <div className="flat-list col-sm-7">
+        {this.props.flats.map((flat) => <Flat flat={flat} key={flat.name}/>)}
       </div>
     );
   }
--- src/containers/map.jsx
+++ src/containers/map.jsx
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 
 class Map extends Component {
   render() {
+
+    const style = {
+      height: "100vh"
+    };
+
     return (
-      <div className="col-sm-5">
+      <div className="col-sm-5" style={style}>
         TODO
 
       </div>
