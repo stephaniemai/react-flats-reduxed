e68a13b66cc626655e845501be6974aa7eab0274
stephaniemai <stephaniemai.jacques@gmail.com>
2019-05-09 16:31:43 +0200

    structuring the app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,42 +1,18 @@
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
-import flats from '../../data/flats';
-import FlatList from './flat_list';
-import Marker from './marker';
+import Map from '../containers/map';
+import FlatList from '../containers/flat_list';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      flatList: flats,
-      selectedFlat: { },
-      center: {
-        lat: 48.853372,
-        lng: 2.349175
-      },
-      zoom: 12
-    };
-  }
-
-  selectFlat = (flat) => {
-    this.setState({
-      selectedFlat: flat
-    });
   }
 
   render() {
     return (
-      <div className="container">
-        <FlatList flatList={this.state.flatList} selectFlat={this.selectFlat} />
-        <div className="map-container">
-          <GoogleMapReact
-            defaultCenter={this.state.center}
-            defaultZoom={this.state.zoom}
-          >
-            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
-          </GoogleMapReact>
+      <div className="row">
+        <FlatList  />
+        <Map />
         </div>
-      </div>
     );
   }
 }
--- walkthrough.md
+++ walkthrough.md
@@ -1,4 +1,9 @@
-##
+##Create the structure of the app
+1. In index.jsx, import the app
+2. In app.jsx, the render method will include Flatlist and Map
+3. Create the Flatlist and Map containers
+4. index.html, add the CSS "container-fluid" class, then the "row" class to the rendered app and "col-sm-7" for the flatlist and "col-sm-5" for the map
+
 
 
 
